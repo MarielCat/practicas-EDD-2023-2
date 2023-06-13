0820e5c4727204483ac39a85a534061cc4e4a30b
MarielCat <marromero@ciencias.unam.mx>
2023-06-13 14:44:38 -0600

    Clase Complejidad.java agregada

---
+++ practica-1/src/ed/complejidad/Complejidad.java
@@ -0,0 +1,28 @@
+package ed.complejidad;
+
+public class Complejidad implements IComplejidad{
+
+    public long leeContador(){
+        return 0;
+    }
+
+    /*El método escribeOperaciones y escribeLineaVacía al ser estaticos se implementan dentro de IComplejidad 
+    por lo que no es necesario implementarlos aquí.*/
+
+    public int tPascalRec(int ren, int col){
+        return 0;
+    }
+
+    public int tPascalIt(int ren, int col){
+        return 0;
+    }
+
+    public int fibonacciRec(int n){
+        return 0;
+    }
+
+    public int fibonacciIt(int n){
+        return 0;
+    }
+
+}
